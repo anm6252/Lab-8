6e69074e9e2a3c821151cd605a46d724c54eb627
anm6252 <87391025+anm6252@users.noreply.github.com>
2021-08-26 09:05:49 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -6,12 +6,12 @@ public class Driver {
 		ArrayQ arrayQ = new ArrayQ();
 
 
-		arrayQ.enqueue(1);
-		arrayQ.enqueue(7);
-		arrayQ.enqueue(3);
-		arrayQ.enqueue(4);
-		arrayQ.enqueue(9);
-		arrayQ.enqueue(2);
+			arrayQ.enqueue(1);
+			arrayQ.enqueue(7);
+			arrayQ.enqueue(3);
+			arrayQ.enqueue(4);
+			arrayQ.enqueue(9);
+			arrayQ.enqueue(2);
 
 		
 		while (!arrayQ.isEmpty()) {
@@ -27,12 +27,12 @@ public class Driver {
 		LinkedQueue linkedQueue = new LinkedQueue();
 		
 
-		linkedQueue.enqueue(1);
-		linkedQueue.enqueue(7);
-		linkedQueue.enqueue(3);
-		linkedQueue.enqueue(4);
-		linkedQueue.enqueue(9);
-		linkedQueue.enqueue(2);
+			linkedQueue.enqueue(1);
+			linkedQueue.enqueue(7);
+			linkedQueue.enqueue(3);
+			linkedQueue.enqueue(4);
+			linkedQueue.enqueue(9);
+			linkedQueue.enqueue(2);
 
 		
 			while (!linkedQueue.isEmpty()) {
@@ -47,15 +47,15 @@ public class Driver {
 		LinkedQueue linkedQueue2 = new LinkedQueue();
 		
 
-		linkedQueue2.enqueue(5);
-		linkedQueue2.enqueue(6);
-		linkedQueue2.enqueue(90);
-		linkedQueue2.enqueue(21);
-		linkedQueue2.enqueue(1);
-		linkedQueue2.enqueue(14);
-		linkedQueue2.enqueue(0);
+			linkedQueue2.enqueue(5);
+			linkedQueue2.enqueue(6);
+			linkedQueue2.enqueue(90);
+			linkedQueue2.enqueue(21);
+			linkedQueue2.enqueue(1);
+			linkedQueue2.enqueue(14);
+			linkedQueue2.enqueue(0);
 
-			linkedQueue2.removeMiddle();
+				linkedQueue2.removeMiddle();
 			
 		System.out.println("Number Removed: 21");
 		
